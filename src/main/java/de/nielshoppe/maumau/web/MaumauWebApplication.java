906353e3d19d914c47b62a193d9eb09a92a4f426
Niels Hoppe <mail@nielshoppe.de>
2024-05-10 13:56:51 +0200

    Initial commit

---
+++ src/main/java/de/nielshoppe/maumau/web/MaumauWebApplication.java
@@ -0,0 +1,13 @@
+package de.nielshoppe.maumau.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaumauWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaumauWebApplication.class, args);
+	}
+
+}
